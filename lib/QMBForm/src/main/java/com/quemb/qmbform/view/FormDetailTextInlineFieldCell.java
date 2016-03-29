97dca5c12c5ff3d5dfe5cc6072b730edcdeddbf7
Alvin <didiao.zeng@foxmail.com>
2016-03-29 21:48:41 +0800

    update

--- lib/QMBForm/src/main/java/com/quemb/qmbform/view/FormDetailTextInlineFieldCell.java
+++ lib/QMBForm/src/main/java/com/quemb/qmbform/view/FormDetailTextInlineFieldCell.java
@@ -7,6 +7,9 @@ import com.quemb.qmbform.descriptor.Value;
 import android.content.Context;
 import android.widget.TextView;
 
+import java.lang.reflect.Method;
+import java.util.List;
+
 /**
  * Created by pmaccamp on 9/4/2015.
  */
@@ -44,12 +47,42 @@ public class FormDetailTextInlineFieldCell extends FormTitleFieldCell {
         if (value != null && value.getValue() != null) {
             if (value.getValue() instanceof String) {
                 getDetailTextView().setText((String) value.getValue());
+            } else if(value.getValue() instanceof List) {
+                List<Object> objects = (List<Object>) value.getValue();
+                String valueStr = "";
+                int index = 0;
+                for (Object object : objects) {
+                    String str = String.valueOf(object);
+                    Class clazz = object.getClass();
+                    boolean find = false;
+                    try {
+                        Method m1 = clazz.getDeclaredMethod("getName");
+                        str = (String) m1.invoke(object);
+                        find = true;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    if(!find) {
+                        try {
+                            Method m1 = clazz.getDeclaredMethod("getTitle");
+                            str = (String) m1.invoke(object);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    valueStr += str;
+                    if (index != objects.size() - 1) {
+                        valueStr += ",";
+                    }
+                    index ++;
+                }
+                getDetailTextView().setText(valueStr);
             } else {
                 getDetailTextView().setText(String.valueOf(value.getValue()));
-
             }
         }
-
     }
 
     public TextView getDetailTextView() {
