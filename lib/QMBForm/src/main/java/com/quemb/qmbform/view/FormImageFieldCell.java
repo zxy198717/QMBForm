7b548cb0b5779fd25358be50c5339373ce751c41
Alvin <didiao.zeng@foxmail.com>
2016-08-07 10:54:01 +0800

    build tool update

--- lib/QMBForm/src/main/java/com/quemb/qmbform/view/FormImageFieldCell.java
+++ lib/QMBForm/src/main/java/com/quemb/qmbform/view/FormImageFieldCell.java
@@ -84,20 +84,7 @@ public class FormImageFieldCell extends FormTitleFieldCell {
             }
         };
 
-        String[] items = {"拍照", "相册", "取消"};
-
-        new AlertDialog.Builder(getContext())
-                .setItems(items, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (which == 0) {
-                            mImagePickerManager.takePicture(crop, ls);
-                        } else if (which == 1){
-                            mImagePickerManager.chooseImage(crop, ls);
-                        }
-                    }
-                })
-                .show();
+        mImagePickerManager.pickImage(crop, ls);
     }
 
     @Override
